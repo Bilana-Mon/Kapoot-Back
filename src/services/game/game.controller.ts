27540cca0297a78dbe343fc0ed55b3783ea06499
Bilana-Mon <bilanamoon@gmail.com>
2022-07-01 19:06:46 +0300

    fixed game controller

--- src/services/game/game.controller.ts
+++ src/services/game/game.controller.ts
@@ -1,4 +1,71 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller,
+    Get,
+    Post,
+    Put,
+    Param,
+    Body,
+    Delete
+ } from '@nestjs/common';
+ import { GameService } from './game.service';
+ import {Game as GameModel} from "@prisma/client";
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+    constructor
+        (
+            private readonly gameService: GameService
+        ) { }
+    
+  @Get('/:id')
+  async getGameById(@Param('id') id: string): Promise<GameModel> {
+    return this.gameService.game({ id: Number(id) });
+  }
+
+  @Get('/games')
+  async getPlayedGames(): Promise<GameModel[]> {
+    return this.gameService.games({
+      where: {},
+    });
+  }
+
+  @Get('/filtered-games/:searchString')
+  async getFilteredGames(
+    @Param('searchString') searchString: string,
+  ): Promise<GameModel[]> {
+    return this.gameService.games({
+      where: {
+        OR: [
+          {
+            themes: {},
+          },
+          {
+            finished: {},
+          },
+        ],
+      },
+    });
+  }
+
+  @Post()
+  async createGame(
+    @Body() gameData: {  adminId: number, started: Date, finished: Date, players:number },
+  ): Promise<any> {
+    const { adminId, started, finished, players } = gameData;
+  }
+
+  
+
+  @Put('/start-game/:id')
+  async startPost(@Param('id') id: string): Promise<GameModel> {
+    return this.gameService.updateGame({
+      where: { id: Number(id) },
+      data: { started: new Date },
+    });
+  }
+
+  @Delete('/:id')
+  async deleteGame(@Param('id') id: string): Promise<GameModel> {
+    return this.gameService.deleteGame({ id: Number(id) });
+  }
+}
