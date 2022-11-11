ef6b357e3a71fd143339fe031f90f37dc014c723
Bilana-Mon <bilanamoon@gmail.com>
2022-11-11 16:50:41 +0200

    fixed me endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,6 +14,7 @@ import {
 import { UserService } from '../services/user/user.service';
 import { User as UserModel } from '@prisma/client';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -40,21 +41,10 @@ export class AuthController {
         }
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/me')
-    async getUserByToken(@Request() req, @Body() loginPayload: { nickname: string; password: string }) {
-        const token = await this.authService.loginUser(loginPayload);
-        if (!token) throw new UnauthorizedException('No token provided!');
-
-        const userId = await this.authService.getUserIdByToken(token);
-        console.log(userId, 'userId');
-
-
-        const foundUser = await this.authService.findUserByToken(userId);
-        console.log(foundUser);
-
-        const nickname = foundUser.nickname;
-        console.log(nickname);
-
-        return { userId, nickname };
+    async getUserByToken(@Request() req) {
+        const userId = req.user.userId;
+        return this.userService.getUserById(userId);
     }
 }
