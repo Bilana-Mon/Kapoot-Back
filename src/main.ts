302cb874b56a9b8794a12231fdc814e403eec433
Bilana-Mon <bilanamoon@gmail.com>
2022-11-19 15:45:37 +0200

    changed port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('PORT');
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(port);
 }
+
 bootstrap();
+
